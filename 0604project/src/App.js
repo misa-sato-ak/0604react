e6058482b4a30bb00de856a180cc7a732662330f
misa-sato-ak <akmisa519@gmail.com>
2024-06-05 11:35:24 +0900

    0605

--- 0604project/src/App.js
+++ 0604project/src/App.js
@@ -3,8 +3,29 @@ import './App.css';
 import ReviewCard from './ReviewCard';
 
 const App = () => {
+  const reviews = [
+    {
+      id: 1,
+      author: 'ReactFan91',
+      content: 'React is an amazing library.It makes building web applications so much easier.',
+    },
+    {
+      id: 2,
+      author: 'JSNinja2000',
+      content: 'JavaScript is the language of the web. With React, it becomes even more powerful.',
+    },
+    {
+      id: 3,
+      author: 'ComponentMaster',
+      content: 'Reusable components are a game-changer. React makes component-based development a breeze.',
+    },
+  ];
   return (
     <div className="app-container">
+      <h1>レビューカード</h1>
+      {reviews.map((review) => (
+        <ReviewCard key={review.id} review={review} />
+      ))}
     </div>
     );
 };
